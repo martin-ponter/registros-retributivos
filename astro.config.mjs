f0036d9f3ec0db97c69eb498597073ec2096d292
altarialights <altarialights@gmail.com>
2026-06-01 09:49:57 +0200

    primer commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,9 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
     output: "server",
     adapter: vercel(),
+    security: {
+        checkOrigin: false,
+    },
     integrations: [react()],
     vite: {
         plugins: [tailwindcss()],
